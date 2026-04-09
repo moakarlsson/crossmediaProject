ee1ede5e9b779145d5f4d791603ca9a074a0f94d
Moakarlssonns <mo-karlsson@hotmail.com>
2026-04-09 12:39:44 +0200

    Added test route for basic functionality

--- app.js
+++ app.js
@@ -2,8 +2,9 @@
 // import tasksRouter from "./routes/tasks.js"
 //app.use('/tasks', tasksRouter)
 import express from "express";
+import testRouter from "./routes/test.js";
 const app = express(); 
-
+app.use('/', testRouter);
 app.use(express.json());
 
 export default app;
